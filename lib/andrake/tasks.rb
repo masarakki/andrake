03aa5b1f351b64b8e1def7b2b4f9e03bb64149f6
Yamada Masaki <masaki@hisme.net>
2011-06-21 05:31:37 +0900

    enable to show tasks about version by rake -T

--- lib/andrake/tasks.rb
+++ lib/andrake/tasks.rb
@@ -3,7 +3,7 @@ require 'rake/tasklib'
 
 class Rake::Application
   def andrake
-    
+
   end
 end
 
@@ -11,21 +11,30 @@ class Andrake::Tasks < ::Rake::TaskLib
   def initialize
     define
   end
-  
+
   private
   def define
     namespace :version do
       namespace :bump do
+        desc "bump major version"
         task :major do
-          puts 'major update'
-        end
-        
-        task :minor do
-          puts 'minor update'
+          manifest = Andrake::Manifest.load
+          manifest.bump_major
+          puts "major update to #{manifest.version} (not implemented)"
         end
 
+        desc "bump minor version"
+        task :minor do
+          manifest = Andrake::Manifest.load
+          manifest.bump_minor
+          puts "minor update to #{manifest.version} (not implemented)"
+        end
+
+        desc "bump patch version"
         task :patch do
-          puts 'patch update'
+          manifest = Andrake::Manifest.load
+          manifest.bump_patch
+          puts "patch update #{manifest.version} (not implemented)"
         end
       end
     end
