02f1205bd0a58a5c1e48582d7136cbaff781c274
Yamada Masaki <masaki@hisme.net>
2011-06-21 03:36:26 +0900

    change scope of before

--- spec/manifest/version_spec.rb
+++ spec/manifest/version_spec.rb
@@ -1,9 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Andrake::Manifest do
-  before do
+  before(:all) do
     @manifest = File.read(File.expand_path(File.dirname(__FILE__) + '/AndroidManifest.xml'))
   end
+
   subject { Andrake::Manifest.new(@manifest) }
   its(:version) { "0.8.1" }
   its(:version_major) { should eq(0) }
