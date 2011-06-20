d1bca571b19df699ffbf285e862dab7617b09286
Yamada Masaki <masaki@hisme.net>
2011-06-21 04:27:42 +0900

    add spec for bumpup versions

--- spec/manifest/version_spec.rb
+++ spec/manifest/version_spec.rb
@@ -11,4 +11,44 @@ describe Andrake::Manifest do
   its(:version_minor) { should eq(8) }
   its(:version_patch) { should eq(1) }
   its(:version_code) { should eq(25) }
+
+  describe "#bump_major" do
+    subject do
+      manifest = Andrake::Manifest.new(@manifest)
+      manifest.bump_major
+      manifest
+    end
+
+    its(:version_major) { should eq(1) }
+    its(:version_minor) { should eq(0) }
+    its(:version_patch) { should eq(0) }
+    its(:version_code) { should eq(26) }
+  end
+
+  describe "#bump_minor" do
+    before(:all) do
+      @manifest = Andrake::Manifest.new(@manifest)
+      @manifest.bump_minor
+    end
+
+    subject { @manifest }
+
+    its(:version_major) { should eq(0) }
+    its(:version_minor) { should eq(9) }
+    its(:version_patch) { should eq(0) }
+    its(:version_code) { should eq(26) }
+  end
+
+  describe "#bump_patch" do
+    subject do
+      manifest = Andrake::Manifest.new(@manifest)
+      manifest.bump_patch
+      manifest
+    end
+
+    its(:version_major) { should eq(0) }
+    its(:version_minor) { should eq(8) }
+    its(:version_patch) { should eq(2) }
+    its(:version_code) { should eq(26) }
+  end
 end
