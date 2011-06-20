63ac22595229565ed2a7c3bc46ec9fc4452cdfd1
Yamada Masaki <masaki@hisme.net>
2011-06-21 05:02:38 +0900

    be meta

--- lib/andrake/manifest/version.rb
+++ lib/andrake/manifest/version.rb
@@ -3,29 +3,44 @@ module Andrake::Manifest::Version
     [version_major, version_minor, version_patch].join(".")
   end
 
-  def version_major
-    load_version unless @version_major
-    @version_major
+  def bump_major
+    self.version_major += 1
+    self.version_minor = 0
+    self.version_patch = 0
+    self.version_code += 1
   end
 
-  def version_minor
-    load_version unless @version_minor
-    @version_minor
+  def bump_minor
+    self.version_minor += 1
+    self.version_patch = 0
+    self.version_code += 1
   end
 
-  def version_patch
-    load_version unless @version_patch
-    @version_patch
+  def bump_patch
+    self.version_patch += 1
+    self.version_code += 1
   end
 
-  def version_code
-    @version_code ||= @document.xpath('/manifest').first["versionCode"].to_i
+  [:major, :minor, :patch, :code].each do |element|
+    val_name = "@version_#{element}".to_sym
+
+    define_method("version_#{element}".to_sym) do
+      load_version unless instance_variable_get(val_name)
+      instance_variable_get(val_name)
+    end
+
+    define_method("version_#{element}=".to_sym) do |val|
+      instance_variable_set(val_name, val)
+      val
+    end
+
+    protected "version_#{element}=".to_sym
   end
 
-
-
-  private
   def load_version
-    @version_major, @version_minor, @version_patch = @document.xpath('/manifest').first["versionName"].split(/\./).map(&:to_i)
+    manifest = @document.xpath('/manifest').first
+    @version_major, @version_minor, @version_patch = manifest["versionName"].split(/\./).map(&:to_i) + [0, 0, 0]
+    @version_code = manifest["versionCode"].to_i
+    true
   end
 end
